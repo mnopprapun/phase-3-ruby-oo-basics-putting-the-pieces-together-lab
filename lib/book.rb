3090d97fdbb3ad8b1e6bca9b7db566d87c24851d
Marc Nopprapun <mnopprapun@gmail.com>
2021-05-19 15:26:08 -0500

    commit tree aa407953dfd34f5527aa821f42c7dbcd4420d049

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,48 @@
+require 'pry'
 
 
+class Book
+
+
+	def initialize(title)
+		# instance variable attaches to each instance
+		# binding.pry
+		@title = title
+		@author = author
+	  end
+
+	#   attr_reader :title
+	def title
+	@title
+	end
+
+	def author
+		@author
+	end
+
+def page_count
+	@page_count
+end
+
+def genre
+	@genre
+end
+
+	#   attr_accessor :author, :page_count, :genre
+def author=(author_name)
+	@author = author_name
+end
+
+def page_count=(page_count)
+	@page_count = page_count
+end 
+
+def genre=(genre)
+	@genre=genre
+end
+
+def turn_page
+	puts "Flipping the page...wow, you read fast!"
+end
+
+	end
